244db6e598f3b667d9e82b40e3c65e14fc58a336
Ian Chen <ichen@openrobotics.org>
2023-08-25 14:12:06 -0700

    Update wave shaders for vulkan  (#3)
    * fix shaders for vulkan
    
    
    ---------
    
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Usage:
 ```
 git clone https://github.com/gazebosim/harmonic_demo
 
+cd harmonic_demo
+
 export GZ_SIM_RESOURCE_PATH=`pwd`/harmonic_demo:$GZ_SIM_RESOURCE_PATH
 
 gz sim -v 4 "harmonic_demo/harmonic.sdf"
--- harmonic_demo/Coast Waves 2/materials/programs/GerstnerWaves_vs_330.glsl
+++ harmonic_demo/Coast Waves 2/materials/programs/GerstnerWaves_vs_330.glsl
@@ -32,32 +32,40 @@
 //
 // The derivative terms (Tangent, Binormal, Normal) have been
 // updated to be consistent with this convention.
-//
 
-#version 330
 
-in vec4 vertex;
-in vec4 uv0;
-uniform mat4 worldviewproj_matrix;
+// original concept:
+// https://developer.nvidia.com/gpugems/gpugems/part-i-natural-effects/chapter-1-effective-water-simulation-physical-models
 
-/////////// Input parameters //////////
-// Waves
-uniform int Nwaves;
-uniform vec3 camera_position_object_space;
-uniform float rescale;
-uniform vec2 bumpScale;
-uniform vec2 bumpSpeed;
-uniform float t;
-uniform vec3 amplitude;
-uniform vec3 wavenumber;
-uniform vec3 omega;
-uniform vec3 steepness;
-uniform vec2 dir0;
-uniform vec2 dir1;
-uniform vec2 dir2;
-uniform float tau;
+
+#version ogre_glsl_ver_330
+
+vulkan_layout( OGRE_POSITION ) in vec4 vertex;
+vulkan_layout( OGRE_TEXCOORD0 ) in vec4 uv0;
+
+vulkan( layout( ogre_P0 ) uniform Params { )
+  uniform mat4 worldviewproj_matrix;
+
+  /////////// Input parameters //////////
+  // Waves
+  uniform int Nwaves;
+  uniform vec3 camera_position_object_space;
+  uniform float rescale;
+  uniform vec2 bumpScale;
+  uniform vec2 bumpSpeed;
+  uniform float t;
+  uniform vec3 amplitude;
+  uniform vec3 wavenumber;
+  uniform vec3 omega;
+  uniform vec3 steepness;
+  uniform vec2 dir0;
+  uniform vec2 dir1;
+  uniform vec2 dir2;
+  uniform float tau;
+vulkan( }; )
 
 /////////// Output variables to fragment shader //////////
+vulkan_layout( location = 0 )
 out block
 {
   mat3 rotMatrix;
@@ -77,8 +85,6 @@ struct WaveParameters {
   vec2 d;       // horizontal direction of wave
   float q;      // steepness for Gerstner wave (q=0: rolling sine waves)
 };
-
-
 
 out gl_PerVertex
 {
